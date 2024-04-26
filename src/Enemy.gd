e7fbf7ca0ccdbb93c50d04a2f132b429e4571853
therockerline <therockerline@gmail.com>
2024-04-27 01:56:11 +0200

    update

--- src/Enemy.gd
+++ src/Enemy.gd
@@ -3,8 +3,16 @@ extends PathFollow2D
 
 @export var path: Path2D
 @export var speed: float = 10.0
+@export var max_life = 5.0
+@export var life = 5.0
+@onready var sprite = $Sprite2D
+@onready var life_rect = $Node2D/LifeRect
+@onready var dmg_rect = $Node2D/DmgRect
+signal death
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(d):
+	sprite.look_at(path.curve.get_point_position(progress))
+	#($Node2D as Control).rotation_degrees = 90
 	if path.curve.get_baked_length() > 1:
 		var totalDistance = path.curve.get_baked_length()
 		var ratio_at_second = speed / totalDistance
@@ -12,3 +20,14 @@ func _process(d):
 		progress_ratio += delta_ratio_at_second
 		if progress_ratio == 1.0:
 			queue_free()
+
+func shot(dmg: float):
+	var l = life - dmg
+	life = l if l > 0 else 0
+	if life == 0:
+		dmg_rect.scale.x = 1
+		queue_free()
+		death.emit()
+	else:
+		var perc = 1.0 -  ((max_life * life) / life_rect.size.x)
+		dmg_rect.scale.x = perc
--- src/Tower.gd
+++ src/Tower.gd
@@ -1,15 +1,27 @@
 class_name Tower
 extends Node2D
 
+enum TowerTypes {
+	AREA,
+	SHOOTER
+}
 
-
+@export var damage := 1.0
 @onready var image: Sprite2D = $Image
 @onready var area: CollisionShape2D = $TargetArea/CollisionShape2D
 @onready var ray_container: Node2D = $RayContainer
 @onready var fire_timer: Timer = $FireTimer
 @onready var tower_ray_target_prop= load("res://scenes/entity/TowerRayTraget.tscn") 
 var ray_dictionary: Dictionary = {}
-var tower_type: Enums.TowerTypes = 
+var tower_type: TowerTypes = TowerTypes.SHOOTER
+
+var near = {}
+var far = {}
+var first = {}
+var last = {}
+var strong = {}
+var weak = {}
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -30,7 +42,20 @@ func create_ray(enemy: Enemy):
 		fire_timer.start()
 
 func remove_ray(enemy: Enemy):
-	ray_dictionary[enemy.name]['ray'].queue_free()
+	var ray_item = ray_dictionary[enemy.name]
+	if ray_item == near:
+		near = {}
+	if ray_item == near:
+		far = {}
+	if ray_item == first:
+		first = {}
+	if ray_item == last:
+		last = {}
+	if ray_item == strong:
+		strong = {}
+	if ray_item == weak:
+		weak = {}
+	ray_item.ray.queue_free()
 	ray_dictionary.erase(enemy.name)
 	if len(ray_dictionary.values()) == 0 and not fire_timer.is_stopped():
 		fire_timer.stop()
@@ -46,7 +71,37 @@ func _on_target_area_area_exited(area):
 	remove_ray(enemy)
 	pass # Replace with function body.
 
-func _on_fire_timer_timeout():
+func reset_or_set_modulate(a: Dictionary, b: Dictionary, modulate: Color):
+	if a.has('ray'):
+		if a != b and is_instance_valid(a.ray):
+			a.ray.modulate = Color.WHITE
+		if b.has('ray') and is_instance_valid(b.ray):
+			b.ray.modulate = modulate
+	return b
+
+func _on_fire_timer_timeout():	
+	var _near: Dictionary = near
+	var _far: Dictionary = far
+	var _strong: Dictionary = strong
+	var _weak: Dictionary = weak
+	var _first: Dictionary = ray_dictionary.values().front()
+	var _last: Dictionary = ray_dictionary.values().back()
+	var ray_len = len(ray_dictionary.keys())
+	for ray_item in ray_dictionary.values():
+		var enemy: Enemy = ray_item.enemy
+		if is_instance_valid(enemy):
+			_far = ray_item if (not _far.has('enemy') or (is_instance_valid(_far.enemy) and position.distance_to(_far.enemy.position) < position.distance_to(enemy.position))) else _far
+			_near = ray_item if (not _near.has('enemy') or (is_instance_valid(_near.enemy) and  position.distance_to(_near.enemy.position) >= position.distance_to(enemy.position))) else _near
+			_strong = ray_item if (not _strong.has('enemy') or (is_instance_valid(_strong.enemy) and (_strong.enemy as Enemy).life > enemy.life)) else _strong
+			_weak = ray_item if (not _weak.has('enemy') or (is_instance_valid(_weak.enemy) and (_weak.enemy as Enemy).life <= enemy.life)) else _weak
 	
-	print('fire')
+	near = reset_or_set_modulate(near, _near, Color.BLUE)
+	far = reset_or_set_modulate(far, _far, Color.GREEN)
+	first = reset_or_set_modulate(first, _first, Color.RED)
+	last = reset_or_set_modulate(last, _last, Color.WEB_PURPLE)
+	strong = reset_or_set_modulate(strong, _strong, Color.YELLOW)
+	weak = reset_or_set_modulate(weak, _weak, Color.GRAY)
+	
+	if strong.has('enemy'):
+		(strong.enemy as Enemy).shot(damage)
 	pass # Replace with function body.
